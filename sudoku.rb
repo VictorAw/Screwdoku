104600f24519516838f193ed067d187c54bf1564
App Academy Student <student@appacademy.io>
2016-09-15 13:50:29 -0700

    Make sudoku play. Set board properly. Replace rows alias with a proper 'rows' method. Remove random number addition. Remove +1 from position entry. Properly return value argument.

--- sudoku.rb
+++ sudoku.rb
@@ -1,5 +1,6 @@
 require_relative "board"
 require 'colorize'
+require 'byebug'
 
 puts "Only contractors write code this bad.".yellow
 
@@ -10,15 +11,15 @@ class SudokuGame
   end
 
   def initialize(board)
-    @board = [[]]
+    @board = board
   end
 
   def method_missing(method_name, *args)
     if method_name =~ /val/
-      Integer(1)
+      Integer(args[0])
     else
       string = args[0]
-      string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
+      string.split(",").map! { |char| Integer(char) }
     end
   end
 
@@ -30,7 +31,9 @@ class SudokuGame
 
       begin
         pos = parse_pos(gets.chomp)
-      rescue
+      rescue #Exception => ex
+        # debugger
+        # puts "The error is of type #{ex.class}"
         # TODO: Google how to print the error that happened inside of a rescue statement.
         puts "Invalid position entered (did you use a comma?)"
         puts ""
@@ -83,5 +86,5 @@ class SudokuGame
   attr_reader :board
 end
 
-
 game = SudokuGame.from_file("puzzles/sudoku1.txt")
+game.run
